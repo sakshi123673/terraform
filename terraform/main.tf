f4bc9a92d3511f065dc25ef44457f7e159f3411b
root <root@ip-172-31-0-212.ap-southeast-2.compute.internal>
2023-10-21 13:54:54 +0000

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+region = "ap-southeast-2"
+access_key = "AKIA2VGJUPMDHHK3WKHF"
+secret_key = "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"
+}
+
+resource "aws_instance" "key" {
+ami = "ami-023e1b810cda5ce69"
+instance_type = "t2.micro"
+key_name = "mykey1"
+vpc_security_group_ids = [aws_security_group.demo-sg.id]
+tags = {
+Name = "project_instance"
+}
+}
